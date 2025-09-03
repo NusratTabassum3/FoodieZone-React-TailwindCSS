7661721a9198f7e483fc77743ac13d263dc80dc8
NusratTabassum3 <tabassumnusrat3@gmail.com>
2025-09-03 23:59:52 +0600

    Create Service Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Footer from "./components/Footer/Footer";
 import CoverBanner from "./components/CoverBanner/CoverBanner";
+import Service from "./components/Services/Services";
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Service />
       <CoverBanner />
       <Footer />
     </div>
