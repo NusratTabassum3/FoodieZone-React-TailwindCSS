962ace1b9d264ce9921391045c996f7d50ff1480
NusratTabassum3 <tabassumnusrat3@gmail.com>
2025-09-03 23:57:10 +0600

    Create Banner Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Footer from "./components/Footer/Footer";
+import CoverBanner from "./components/CoverBanner/CoverBanner";
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <CoverBanner />
       <Footer />
     </div>
   );
