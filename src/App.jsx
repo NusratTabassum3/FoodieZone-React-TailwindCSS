8cf41d98b8f3dff297c8b0b7b95bd59220797cb0
NusratTabassum3 <tabassumnusrat3@gmail.com>
2025-09-05 23:22:03 +0600

    Create Banner Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Footer from "./components/Footer/Footer";
 import CoverBanner from "./components/CoverBanner/CoverBanner";
 import Service from "./components/Services/Services";
 import AppStore from "./components/AppStore/AppStore";
+import Banner from "./components/Banner/Banner";
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
       <Service />
+      <Banner />
       {/* <CoverBanner /> */}
       <AppStore />
       <Footer />
