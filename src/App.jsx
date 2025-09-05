a64ca8426ca6fe4d67fed7b5b26a5d956b360b98
NusratTabassum3 <tabassumnusrat3@gmail.com>
2025-09-05 23:18:32 +0600

    Create Appstore Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Hero from "./components/Hero/Hero";
 import Footer from "./components/Footer/Footer";
 import CoverBanner from "./components/CoverBanner/CoverBanner";
 import Service from "./components/Services/Services";
+import AppStore from "./components/AppStore/AppStore";
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
       <Service />
-      <CoverBanner />
+      {/* <CoverBanner /> */}
+      <AppStore />
       <Footer />
     </div>
   );
